d84c2603332bb1087fa72e8315a8c5ac5bb06530
Gustavo Parreira <gustashminecraft@gmail.com>
2015-08-22 22:52:27 +0100

    Support for Android API 23 (Android Marshmallow 6.0)

--- RTMChat/app/src/main/java/config/Config.java
+++ RTMChat/app/src/main/java/config/Config.java
@@ -5,8 +5,8 @@ public class Config {
 	final public static String CLUSTERURL = "http://ortc-developers.realtime.co/server/2.1/";
 	final public static String METADATA = "androidApp";
 	final public static String TOKEN = "token";
-	final public static String APPKEY = "YOUR_APPLICATION_KEY";
-    final public static String PROJECT_ID = "YOUR_PROJECT_ID";
+	final public static String APPKEY = "YOUR_REALTIME_APPLICATION_KEY";
+    final public static String PROJECT_ID = "YOUR_GOOGLE_PROJECT_NUMBER";
 
 
     final public static int CHANNEL_NAME = 1;

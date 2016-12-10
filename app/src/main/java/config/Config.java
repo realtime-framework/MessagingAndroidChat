9f7adbc2f48a4e682895848e8c5432fde50c643a
Realtime Framework <framework@realtime.co>
2016-12-10 21:43:01 +0000

    Update Config.java

--- app/src/main/java/config/Config.java
+++ app/src/main/java/config/Config.java
@@ -3,12 +3,11 @@ package config;
 public final class Config {
 
 	final public static String CLUSTERURL = "http://ortc-developers.realtime.co/server/2.1/";
-    //final public static String CLUSTERURL = null;
-    final public static String URL = null;
+    	final public static String URL = null;
 	final public static String METADATA = "androidApp";
 	final public static String TOKEN = "token";
 	final public static String APPKEY = "YOUR_APPLICATION_KEY";
-    final public static String PROJECT_ID = "YOUR_GOOGLE_PROJECT_NUMBER";
+    	final public static String PROJECT_ID = "YOUR_FIREBASE_SENDER_ID";
 
 
     final public static int CHANNEL_NAME = 1;
